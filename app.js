5228c2c6bc329fd9dc8ca2ebe6270f2ce5a3489c
sajib689 <jibonraja11@gmail.com>
2023-03-27 11:13:28 +0600

    last

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const input_filed = document.querySelector('.input-filed');
 const btn = document.querySelector('.btn');
 
-function copyData () {
+function copyData (e) {
+    e.preventDefault();
     input_filed.select();
     input_filed.setSelectionRange(0, 99999);
     navigator.clipboard.writeText(input_filed.value);
